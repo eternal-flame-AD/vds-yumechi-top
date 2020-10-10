9be5f8f26a01e56171d86c0a22640360a83be562
eternal-flame-AD <fu@utexas.edu>
2020-10-09 21:04:36 -0500

    Deploy website - based on d554168aa2c7a0adf51b9b9c2e2565ffbe647316

--- page.js
+++ page.js
@@ -2,3 +2,7 @@ document.loadICMNow = function(id) {
     act = new ActiveIcmJS(id);
     act.projectFile = document.getElementById(id).getAttribute("data-file")
 }
+
+document.loadChemicalView = function(mol, id) {
+    (document.moledit || (document.moledit = {}))[id] = new ChemicalView(mol,id,600,600)
+}
